656fef37f6d6e23b51e8bbd860573d2a70d1e1ed
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-02 09:37:15 +0100

    [day 02] Solve part one

--- src/Day02.hs
+++ src/Day02.hs
@@ -7,16 +7,19 @@ import Text.Regex.TDFA
 main :: IO ()
 main = do
   inputLines <- fmap lines getContents
-  print $ inputLines & parse & partOne
-
-parse = map parseLine
+  print $ inputLines & map parseLine & partOne
 
 parseLine :: String -> PasswordInfo
 parseLine x = PasswordInfo (read x1) (read x2) (head x3) x4
   where (_, _, _, [x1, x2, x3, x4]) =
           x =~ "([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)" :: (String, String, String, [String])
 
-partOne = head
+partOne :: [PasswordInfo] -> Int
+partOne = length . filter isValid
 
 data PasswordInfo = PasswordInfo Int Int Char String
                   deriving (Show, Eq)
+
+isValid :: PasswordInfo -> Bool
+isValid (PasswordInfo n1 n2 c p) = n1 <= n && n <= n2
+  where n = p & filter (== c) & length
