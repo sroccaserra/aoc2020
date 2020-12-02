c87ca2b5dbe4aa04c5a6fc827d8be7a4e7402833
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-02 17:06:38 +0100

    r Reorganize day 02 code

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,5 +1,6 @@
 module Day02 where
 
+import Data.Char
 import Data.Function
 
 main :: IO ()
@@ -7,25 +8,15 @@ main = do
   inputLines <- fmap lines getContents
   print $ inputLines & map parseLine & partTwo
 
-parseLine :: String -> PasswordInfo
-parseLine x = PasswordInfo (read x1 ::Int) (read x2 ::Int) (head x3) x4
-  where [x1, x2, x3, x4] = x & map clean & words
-        clean '-' = ' '
-        clean ':' = ' '
-        clean c = c
-
 partOne :: [PasswordInfo] -> Int
 partOne = length . filter isValidPartOne
-
-partTwo :: [PasswordInfo] -> Int
-partTwo = length . filter isValidPartTwo
-
-data PasswordInfo = PasswordInfo Int Int Char String
-                  deriving (Show, Eq)
 
 isValidPartOne :: PasswordInfo -> Bool
 isValidPartOne (PasswordInfo n1 n2 c p) = n1 <= n && n <= n2
   where n = p & filter (== c) & length
+
+partTwo :: [PasswordInfo] -> Int
+partTwo = length . filter isValidPartTwo
 
 isValidPartTwo :: PasswordInfo -> Bool
 isValidPartTwo (PasswordInfo n1 n2 c p) =
@@ -33,3 +24,13 @@ isValidPartTwo (PasswordInfo n1 n2 c p) =
   where
     c1 = p !! (n1-1)
     c2 = p !! (n2-1)
+
+data PasswordInfo = PasswordInfo Int Int Char String
+                  deriving (Show, Eq)
+
+parseLine :: String -> PasswordInfo
+parseLine x = PasswordInfo (read x1 ::Int) (read x2 ::Int) (head x3) x4
+  where [x1, x2, x3, x4] = x & map clean & words
+        clean c
+          | isDigit c || isAlpha c = c
+          | otherwise = ' '
