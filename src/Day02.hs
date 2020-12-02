64e2dfb2028a26e0e92c5524d08179e13c53a775
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-02 09:45:35 +0100

    [day 02] Solve part two

--- src/Day02.hs
+++ src/Day02.hs
@@ -7,7 +7,7 @@ import Text.Regex.TDFA
 main :: IO ()
 main = do
   inputLines <- fmap lines getContents
-  print $ inputLines & map parseLine & partOne
+  print $ inputLines & map parseLine & partTwo
 
 parseLine :: String -> PasswordInfo
 parseLine x = PasswordInfo (read x1) (read x2) (head x3) x4
@@ -15,11 +15,21 @@ parseLine x = PasswordInfo (read x1) (read x2) (head x3) x4
           x =~ "([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)" :: (String, String, String, [String])
 
 partOne :: [PasswordInfo] -> Int
-partOne = length . filter isValid
+partOne = length . filter isValidPartOne
+
+partTwo :: [PasswordInfo] -> Int
+partTwo = length . filter isValidPartTwo
 
 data PasswordInfo = PasswordInfo Int Int Char String
                   deriving (Show, Eq)
 
-isValid :: PasswordInfo -> Bool
-isValid (PasswordInfo n1 n2 c p) = n1 <= n && n <= n2
+isValidPartOne :: PasswordInfo -> Bool
+isValidPartOne (PasswordInfo n1 n2 c p) = n1 <= n && n <= n2
   where n = p & filter (== c) & length
+
+isValidPartTwo :: PasswordInfo -> Bool
+isValidPartTwo (PasswordInfo n1 n2 c p) =
+  (c1 == c && c2 /= c) || (c1 /= c && c2 == c)
+  where
+    c1 = p !! (n1-1)
+    c2 = p !! (n2-1)
--- src/Day02Spec.hs
+++ src/Day02Spec.hs
@@ -8,5 +8,5 @@ main :: IO ()
 main = hspec $ do
   describe "Parsing a line" $ do
     it "reads a list of numbers" $ do
-      let result = parse ["1-20 a: aaa"]
-      result `shouldBe` [PasswordInfo 1 20 'a' "aaa"]
+      let result = parseLine "1-20 a: aaa"
+      result `shouldBe` PasswordInfo 1 20 'a' "aaa"
