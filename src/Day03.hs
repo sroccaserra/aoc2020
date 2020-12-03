99d1559925fdf1be45793faa61da7028f61f2921
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-03 21:08:15 +0100

    r Move cycle up in day 03

--- src/Day03.hs
+++ src/Day03.hs
@@ -1,7 +1,7 @@
 module Day03 where
 
 main :: IO ()
-main = interact $ show . partTwo . lines
+main = interact $ show . partTwo . (map cycle) . lines
 
 partOne :: Map -> Int
 partOne m = countTrees m (3,1)
@@ -18,6 +18,4 @@ countTrees m (dx,dy) = length $ filter (== '#') $ take n $ map (at m) positions
   where n = div 323 dy
         positions = iterate nextPosition (0,0)
         nextPosition (x, y) = (x+dx, y+dy)
-
-at :: Map -> Position -> Char
-at m (x, y) = (cycle $ m !! y)  !! x
+        at m (x, y) = m !! y !! x
