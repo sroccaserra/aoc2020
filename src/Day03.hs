eb14de8b5070feaf876f8922f6e7e707a3447992
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-03 09:19:11 +0100

    r Inline nextPosition

--- src/Day03.hs
+++ src/Day03.hs
@@ -13,10 +13,15 @@ partOne m = countTrees m (3,1)
 partTwo :: Map -> Int
 partTwo m = foldl (*) 1 $ map (countTrees m) [(1,1), (3,1), (5,1), (7,1), (1,2)]
 
+type Map = [String]
+type Position = (Int, Int)
+type Slope = (Int, Int)
+
 countTrees :: Map -> Slope -> Int
-countTrees m s@(_,dy) = sum $ take n $ map (fromEnum . isTreeAt m) positions
-  where positions = iterate (nextPosition s) (0,0)
-        n = div 323 dy
+countTrees m (dx,dy) = sum $ take n $ map (fromEnum . isTreeAt m) positions
+  where n = div 323 dy
+        positions = iterate nextPosition (0,0)
+        nextPosition (x, y) = (x+dx, y+dy)
 
 isTreeAt :: Map -> Position -> Bool
 isTreeAt m = isTree . at m
@@ -26,10 +31,3 @@ at :: Map -> Position -> Char
 at m (x, y) = m !! y !! x'
   where x' = mod x w
         w = m & head & length
-
-nextPosition :: Slope -> Position -> Position
-nextPosition (dx, dy) (x, y) = (x+dx, y+dy)
-
-type Map = [String]
-type Position = (Int, Int)
-type Slope = (Int, Int)
