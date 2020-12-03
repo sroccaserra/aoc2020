c28bf5d7cf3524ecf45e7af577d6bea4e02d4827
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-03 21:40:39 +0100

    r Use zipWith in day 03

--- src/Day03.hs
+++ src/Day03.hs
@@ -10,12 +10,10 @@ partTwo :: Map -> Int
 partTwo m = foldl (*) 1 $ map (countTrees m) [(1,1), (3,1), (5,1), (7,1), (1,2)]
 
 type Map = [String]
-type Position = (Int, Int)
 type Slope = (Int, Int)
 
 countTrees :: Map -> Slope -> Int
-countTrees m (dx,dy) = length $ filter (== '#') $ take n $ map (at m) positions
-  where n = div (length m) dy
-        positions = iterate nextPosition (0,0)
-        nextPosition (x, y) = (x+dx, y+dy)
-        at m (x, y) = m !! y !! x
+countTrees m (dx,dy) = length $ filter (== '#') $ zipWith (!!) rows xs
+  where rows = map (m !!) ys
+        ys = [0,dy..(length m)-1]
+        xs = [0,dx..]
