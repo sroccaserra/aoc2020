20bdcea740302820fec2cacb723f10904b545cd1
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-04 12:58:07 +0100

    r Extract isCid function & use last instead of !!

--- src/Day04.hs
+++ src/Day04.hs
@@ -9,32 +9,32 @@ main = interact $ show . partTwo . (map parseLine) . lines
 parseLine :: String -> Passport
 parseLine s = Passport (sortedWords !! 0) (sortedWords !! 1) (sortedWords !! 2)
   (sortedWords !! 3) (sortedWords !! 4) (sortedWords !! 5) (sortedWords !! 6)
-  where sortedWords = filter (\w -> 'c' /= w !! 0) $ sort $ words s
+  where sortedWords = sort $ filter (not.isCid) $ words s
+
+isCid ('c':_) = True
+isCid _ = False
 
 partTwo ps = length $ filter isValid ps
+
+data Passport = Passport String String String String String String String
+              deriving (Show)
 
 isValid (Passport byr ecl eyr hcl hgt iyr pid)= isByrValid byr && isEclValid ecl &&
   isEyrValid eyr && isHclValid hcl && isHgtValid hgt && isIyrValid iyr && isPidValid pid
 
 isByrValid byr = 1920 <= n && n <= 2002
-  where n = read v
-        v = (!! 1) $ words s
+  where n = read $ last $ words s
         s = map clean byr
-
-clean ':' = ' '
-clean '#' = ' '
-clean c = c
 
 isEclValid ecl =
   elem ecl ["ecl:amb", "ecl:blu", "ecl:brn", "ecl:gry", "ecl:grn", "ecl:hzl", "ecl:oth"]
 
 isEyrValid eyr = 2020 <= n && n <= 2030
-  where n = read v
-        v = (!! 1) $ words s
+  where n = read $ last $ words s
         s = map clean eyr
 
 isHclValid hcl = (hcl !! 4 == '#') && length v == 6 && all isHcl v
-  where v = (!! 1) $ words s
+  where v = last $ words s
         s = map clean hcl
 
 isHcl c = isDigit c || elem c "abcdef"
@@ -50,13 +50,13 @@ lastN :: Int -> [a] -> [a]
 lastN n xs = drop (length xs - n) xs
 
 isIyrValid iyr = 2010 <= n && n <= 2020
-  where n = read v
-        v = (!! 1) $ words s
+  where n = read $ last $ words s
         s = map clean iyr
 
 isPidValid pid = length v == 9 && all isDigit v
-  where v = (!! 1) $ words s
+  where v = last $ words s
         s = map clean pid
 
-data Passport = Passport String String String String String String String
-              deriving (Show)
+clean ':' = ' '
+clean '#' = ' '
+clean c = c
