1bf17b0047932480c8f5b952dd5cac3cbe1f6699
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-04 14:50:52 +0100

    r Parse values earlier

--- src/Day04.hs
+++ src/Day04.hs
@@ -3,56 +3,42 @@ module Day04 where
 import Data.Char
 import Data.List
 
-main :: IO ()
 main = interact $ show . partTwo . (map parseLine) . lines
 
 parseLine :: String -> Passport
-parseLine s = Passport (sortedWords !! 0) (sortedWords !! 1) (sortedWords !! 2)
-  (sortedWords !! 3) (sortedWords !! 4) (sortedWords !! 5) (sortedWords !! 6)
+parseLine s = Passport (read $ values !! 0) (values !! 1) (read $ values !! 2)
+  (values !! 3) (values !! 4) (read $ values !! 5) (values !! 6)
   where sortedWords = sort $ filter (not.isCid) $ words s
+        values = map (snd . splitAt 4) sortedWords
 
 isCid ('c':_) = True
 isCid _ = False
 
 partTwo ps = length $ filter isValid ps
 
-data Passport = Passport String String String String String String String
+data Passport = Passport Int String Int String String Int String
               deriving (Show)
 
-isValid (Passport byr ecl eyr hcl hgt iyr pid)= isByrValid byr && isEclValid ecl &&
-  isEyrValid eyr && isHclValid hcl && isHgtValid hgt && isIyrValid iyr && isPidValid pid
+isValid (Passport byr ecl eyr hcl hgt iyr pid) =
+  isByrValid byr && isEclValid ecl && isEyrValid eyr && isHclValid hcl &&
+  isHgtValid hgt && isIyrValid iyr && isPidValid pid
 
-isByrValid byr = 1920 <= n && n <= 2002
-  where n = read $ valueFrom byr
+isByrValid n = 1920 <= n && n <= 2002
+isEyrValid n = 2020 <= n && n <= 2030
+isIyrValid n = 2010 <= n && n <= 2020
 
-isEclValid ecl =
-  elem ecl ["ecl:amb", "ecl:blu", "ecl:brn", "ecl:gry", "ecl:grn", "ecl:hzl", "ecl:oth"]
+isEclValid ecl = elem ecl ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
 
-isEyrValid eyr = 2020 <= n && n <= 2030
-  where n = read $ valueFrom eyr
+isHclValid ('#':s) = length s == 6 && all isHexDigit s
+isHclValid _ = False
 
-isHclValid hcl = (hcl !! 4 == '#') && length s == 6 && all isHcl s
-  where s = valueFrom hcl
-
-isHcl c = isDigit c || elem c "abcdef"
-
-isHgtValid hgt = (isSuffixOf "cm" hgt || isSuffixOf "in" hgt) && ok v u
+isHgtValid hgt = elem u ["cm", "in"] && ok v u
   where u = last2 hgt
-        v = read $ filter isDigit hgt ::Int
+        v = read $ filter isDigit hgt
         ok n "cm" = 150 <= n && n <= 193
         ok n "in" = 59 <= n && n <= 76
         ok _ _ = False
 
-isIyrValid iyr = 2010 <= n && n <= 2020
-  where n = read $ valueFrom iyr
-
-isPidValid pid = length s == 9 && all isDigit s
-  where s = valueFrom pid
-
-valueFrom s = last $ words $ map clean s
-
-clean ':' = ' '
-clean '#' = ' '
-clean c = c
+isPidValid s = length s == 9 && all isDigit s
 
 last2 xs = drop (length xs - 2) xs
