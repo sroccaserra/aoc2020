c09eb89d58e7fe83b8b78994e28e5af07162fe41
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-05 10:00:13 +0100

    r Smallify for fun

--- src/Day05.hs
+++ src/Day05.hs
@@ -1,18 +1,16 @@
 module Day05 where
 
-import Data.Bifunctor (bimap)
 import Data.List
 
-main = interact $ show . partTwo . (map parseLine) . lines
+main = interact $ show . partTwo . lines
 
-parseLine = bimap (toDec . map dir) (toDec .map dir) . splitAt 7
-  where dir c | elem c "FL" = 0
-              | otherwise = 1
+partOne xs = maximum $ map idNumber xs
+
+partTwo xs = [minimum ids..maximum ids] \\ ids
+  where ids = map idNumber xs
+
+idNumber s = x*8 + y
+  where (a, b) = splitAt 7 s
+        (x, y) = (toDec $ map toBin a, toDec $ map toBin b)
+        toBin c | elem c "FL" = 0 | otherwise = 1
         toDec = foldl1' $ (+) . (*2)
-
-partOne xs = maximum $ map (idNumber) xs
-
-partTwo xs = head $ [minimum ids..maximum ids] \\ ids
-  where ids = map (idNumber) xs
-
-idNumber (x, y) = x*8 + y
