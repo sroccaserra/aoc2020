5aa8597ef94eb14e7726cea2ec6ce0f3b8fce0ad
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-05 16:09:28 +0100

    r Remove useless multiplication and addition

--- src/Day05.hs
+++ src/Day05.hs
@@ -1,6 +1,6 @@
 module Day05 where
 
-import Data.List
+import Data.List ((\\))
 
 main = interact $ show . partTwo . lines
 
@@ -9,8 +9,6 @@ partOne xs = maximum $ map idNumber xs
 partTwo xs = [minimum ids..maximum ids] \\ ids
   where ids = map idNumber xs
 
-idNumber s = x*8 + y
-  where (a, b) = splitAt 7 s
-        (x, y) = (toDec $ map toBin a, toDec $ map toBin b)
-        toBin c | elem c "FL" = 0 | otherwise = 1
-        toDec = foldl1' $ (+) . (*2)
+idNumber s = toDec $ map toBin s
+  where toBin c | elem c "FL" = 0 | otherwise = 1
+        toDec = foldl1 $ (+) . (*2)
