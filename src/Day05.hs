a7f59b1751553866072ed4f4d9565312ec6dd903
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-05 22:13:29 +0100

    r Add a where to be clearer, still fits in a tweet

--- src/Day05.hs
+++ src/Day05.hs
@@ -2,10 +2,11 @@ module Day05 where
 
 main = interact $ show . part2 . lines
 
-part1 = maximum . ids
+part1 = maximum . idNumbers
 
-part2 xs = sum [minimum $ ids xs..maximum $ ids xs] - (sum $ ids xs)
+part2 xs = sum [minimum ns..maximum ns] - sum ns
+  where ns = idNumbers xs
 
-ids = map $ toDec . toBin
+idNumbers = map $ toDec . toBin
   where toBin = map $ fromEnum . flip elem "BR"
         toDec = foldl1 $ (+) . (*2)
