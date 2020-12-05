3c28a1ad3ee18c0eb30cb392a24d199e75865417
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-05 19:22:35 +0100

    Update template to generate only src or test file

--- day.sh
+++ day.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-sed "s/DayXX/Day$1/" "src/DayXX.hs" > "src/Day${1}.hs"
-sed "s/DayXX/Day$1/" "test/DayXXSpec.hs" > "test/Day${1}Spec.hs"
+if test "$2" != "test"
+then
+    sed "s/DayXX/Day$1/" "src/DayXX.hs" > "src/Day${1}.hs"
+else
+    sed "s/DayXX/Day$1/" "test/DayXXSpec.hs" > "test/Day${1}Spec.hs"
+fi
+
 stack build
