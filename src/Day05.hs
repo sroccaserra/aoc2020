82ca4ea92fd6855273f70f3da6e4dbf0c9b82147
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-05 09:23:14 +0100

    r Use Ints when parsing lines

--- src/Day05.hs
+++ src/Day05.hs
@@ -1,14 +1,13 @@
 module Day05 where
 
 import Data.Bifunctor (bimap)
-import Data.Char (digitToInt)
 import Data.List
 
 main = interact $ show . partTwo . (map parseLine) . lines
 
 parseLine = bimap (map dir) (map dir) . splitAt 7
-  where dir c | elem c "FL" = '0'
-              | otherwise = '1'
+  where dir c | elem c "FL" = 0
+              | otherwise = 1
 
 partOne xs = maximum $ map (idNumber . seat) xs
 
@@ -19,5 +18,4 @@ idNumber (x, y) = x*8 + y
 
 seat = bimap toDec toDec
 
-toDec :: String -> Int
-toDec = foldl' (\acc x -> acc * 2 + digitToInt x) 0
+toDec = foldl' (\acc x -> acc * 2 + x) 0
