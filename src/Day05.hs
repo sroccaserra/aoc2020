4a05accc55d3b3bc71eec7c832029432c33f1b64
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-05 07:13:11 +0100

    r Prepare part two

--- src/Day05.hs
+++ src/Day05.hs
@@ -17,20 +17,21 @@ fblrToUpOrDown 'L' = Lower
 fblrToUpOrDown _ = Upper
 
 partOne :: [BoardingPass] -> Int
-partOne xs = foldr max 0 $ map idNumber seats
-  where seats = map (bimap findRow findColumn) xs
-        findRow = bisectBy 0 127
-        findColumn = bisectBy 0 7
-        idNumber (x, y) = x * 8 + y
+partOne xs = foldr max 0 $ map (idNumber . seat) xs
 
+type Seat = (Int, Int)
 type BoardingPass = ([Direction], [Direction])
 data Direction = Lower | Upper
                deriving (Show)
 
--- 0  127 L -> 0 63   -> x      , (y+1)/2-1
--- 0  127 U -> 64 127 -> (y+1)/2, y
--- 0  63  U -> 32 63  -> (y+1)/2, y
--- 32 63  L -> 32 47  -> x      , x+(y+1-x)/2-1
+idNumber :: Seat -> Int
+idNumber (x, y) = x*8 + y
+
+seat :: BoardingPass -> Seat
+seat bp = bimap findRow findColumn bp
+  where findRow = bisectBy 0 127
+        findColumn = bisectBy 0 7
+
 bisectBy :: Int -> Int -> [Direction] -> Int
 bisectBy x _ [] = x
 bisectBy x y (Upper:rest) = bisectBy (mid x y) y rest
