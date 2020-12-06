928ea9e2ca8f58b2c89d1ef9cabaac3dcd0f1811
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-06 08:54:22 +0100

    r Try to learn Data.Text

--- src/Day06.hs
+++ src/Day06.hs
@@ -1,17 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Day06 where
 
 import Data.List
+import Data.List.Extra
+import qualified Data.Text as T
 
-main = interact $ show . partTwo . lines
+main = interact $ show . partTwo . groupByEmptyLines . trimEnd
+
+groupByEmptyLines = map (map T.unpack) . (map $ T.splitOn "\n") . (T.splitOn "\n\n") . T.pack
 
 partOne = sum . map countAnswers
 
-partTwo = sum . map countValid . map stats . answersByPerson
+partTwo = sum . map countValid . map stats
 
-countAnswers = length . groupBy (==) . sort . (filter $ not . (== ' '))
+countAnswers :: [String] -> Int
+countAnswers = length . groupBy (==) . sort . concat
 
-answersByPerson = map words
-
-stats group = (length group, map length $ groupBy (==) $ sort $ foldl1 (++) group)
+stats :: [String] -> (Int, [Int])
+stats group = (length group, map length $ groupBy (==) $ sort $ concat group)
 
 countValid (x, ys) = length $ filter (== x) ys
