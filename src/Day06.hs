f96b8a20f514dd58b4b08ac495e2d89e646c3333
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-06 07:25:13 +0100

    [day 06] Solve part two

--- src/Day06.hs
+++ src/Day06.hs
@@ -2,11 +2,16 @@ module Day06 where
 
 import Data.List
 
-main = interact $ show . partOne . lines
--- main = do
---   xs <- fmap lines getContents
---   mapM_ print $ partOne xs
+main = interact $ show . partTwo . lines
 
 partOne xs = sum $ map countAnswers xs
 
-countAnswers = length . groupBy (==) . sort
+partTwo xs = sum $ map countValid $ stats $ answersByPerson xs
+
+countAnswers = length . groupBy (==) . sort . (filter $ not . (== ' '))
+
+answersByPerson = map words
+
+stats xs = map (\ group -> (length group, map length $ groupBy (==) $ sort $ foldl1 (++) group)) xs
+
+countValid (x, ys) = length $ filter (== x) ys
