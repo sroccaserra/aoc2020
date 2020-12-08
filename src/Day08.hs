6236e54f3977934e353853baddda23720de90cb0
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-08 07:40:37 +0100

    r Use array instead of list for program

--- src/Day08.hs
+++ src/Day08.hs
@@ -1,11 +1,9 @@
 module Day08 where
 
+import Data.Array
 import qualified Data.Set as S
 
 main = interact $ show . partOne . (map parseLine) . lines
--- main = do
---   xs <- fmap lines getContents
---   mapM_ print $ map parseLine xs
 
 parseLine :: String -> Instruction
 parseLine = parseInstructions . words
@@ -18,24 +16,23 @@ parseLine = parseInstructions . words
         readOp "nop" = Nop
         readOp i = error $ "Invalid instruction: " ++ i
 
-partOne xs = execPrg S.empty $ Machine (cycle xs) 0 0
+partOne xs = execPrg S.empty $ Machine prg 0 0
+  where prg = listArray (0, pred $ length xs) xs
 
 type Instruction = (Operation, Int)
 data Operation = Jmp | Acc | Nop
                deriving (Show)
 
-data Machine = Machine [Instruction] Int Int
+data Machine = Machine (Array Int Instruction) Int Int
 
 instance Show Machine where
-    show (Machine prg pc acc) = show (pc, acc, take 10 prg)
+    show (Machine prg pc acc) = show (pc, acc, prg!pc)
 
 execPrg s m@(Machine _ pc _) | S.member pc s = m
 execPrg s m@(Machine prg pc _) = execPrg (S.insert pc s) $ execInstruction m op n
-  where (op, n) = prg !! pc
+  where (op, n) = prg ! pc
 
 execInstruction :: Machine -> Operation -> Int -> Machine
 execInstruction (Machine prg pc acc) Acc n = (Machine prg (succ pc) $ acc + n)
 execInstruction (Machine prg pc acc) Jmp n = (Machine prg (pc + n) acc)
 execInstruction (Machine prg pc acc) Nop _ = (Machine prg (succ pc) acc)
-
-findInstruction (Machine prg pc _) = prg !! pc
