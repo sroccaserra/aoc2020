fd3a3bcba69b28203957d0c8c6cbdc5408ab0643
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-09 13:58:52 +0100

    r Change Vector import to unqualified

--- src/Day09.hs
+++ src/Day09.hs
@@ -1,24 +1,24 @@
 module Day09 where
 
-import qualified Data.Vector as V
+import Data.Vector (Vector, (!), fromList, ifilter, slice)
 
-main = interact $ show . partTwo . V.fromList . (map read) . lines
+main = interact $ show . partTwo . fromList . (map read) . lines
 
-partOne v = findInvalidNumbers 25 v V.! 0
+partOne v = findInvalidNumbers 25 v ! 0
 
 partTwo v = minimum ns + maximum ns
   where ns = head $ head $ filter (/= []) slices
         slices = map (\i -> findContiguousSum v n i) [0..(length v) - 1]
         n = partOne v
 
-findInvalidNumbers n v = V.ifilter (\i _ -> not $ hasMatchingSum n v $ i+n) candidates
-  where candidates = V.slice n ((length v) - n) v
+findInvalidNumbers n v = ifilter (\i _ -> not $ hasMatchingSum n v $ i+n) candidates
+  where candidates = slice n ((length v) - n) v
 
-previousNs :: Int -> Int -> V.Vector Int -> V.Vector Int
-previousNs n i v = V.slice (i-n) n v
+previousNs :: Int -> Int -> Vector Int -> Vector Int
+previousNs n i v = slice (i-n) n v
 
-hasMatchingSum :: Int -> V.Vector Int -> Int -> Bool
-hasMatchingSum n v i = elem (v V.! i) sums
+hasMatchingSum :: Int -> Vector Int -> Int -> Bool
+hasMatchingSum n v i = elem (v ! i) sums
   where ns = previousNs n i v
         sums = do
           i <- ns
@@ -27,5 +27,5 @@ hasMatchingSum n v i = elem (v V.! i) sums
 
 findContiguousSum v n i = filter ((== n) . sum) $ possibleSlices v i
 
-possibleSlices v i = map (\n -> V.slice (i-n) (n+1) v) ns
+possibleSlices v i = map (\n -> slice (i-n) (n+1) v) ns
   where ns = [0..i]
