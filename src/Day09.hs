1db9eb47c7166320fb57e3dba1a059b5e181e286
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-09 08:57:06 +0100

    [day 09] Solve part two

--- src/Day09.hs
+++ src/Day09.hs
@@ -2,12 +2,17 @@ module Day09 where
 
 import qualified Data.Vector as V
 
-main = interact $ show . partOne . V.fromList . (map read) . lines
+main = interact $ show . partTwo . V.fromList . (map read) . lines
 
 partOne v = findInvalidNumbers 25 v V.! 0
 
+partTwo v = minimum ns + maximum ns
+  where ns = head $ head $ filter (/= []) slices
+        slices = map (\i -> findContiguousSum v n i) [0..(length v) - 1]
+        n = partOne v
+
 findInvalidNumbers n v = V.ifilter (\i _ -> not $ hasMatchingSum n v $ i+n) candidates
-  where candidates = V.slice n ((V.length v) - n) v
+  where candidates = V.slice n ((length v) - n) v
 
 previousNs :: Int -> Int -> V.Vector Int -> V.Vector Int
 previousNs n i v = V.slice (i-n) n v
@@ -19,3 +24,8 @@ hasMatchingSum n v i = elem (v V.! i) sums
           i <- ns
           j <- ns
           return $ i + j
+
+findContiguousSum v n i = filter ((== n) . sum) $ possibleSlices v i
+
+possibleSlices v i = map (\n -> V.slice (i-n) (n+1) v) ns
+  where ns = [0..i]
--- test/Day09Spec.hs
+++ test/Day09Spec.hs
@@ -8,16 +8,28 @@ import Test.Hspec
 
 main = hspec spec
 
-firstExample = V.fromList [ 35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182
-                          , 127, 219 , 299, 277, 309, 576 ]
+anExample = V.fromList [ 35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182 , 127
+                       , 219 , 299, 277, 309, 576 ]
+invalidNumber = 127
+
 spec =
   describe "Day 09" $ do
     it "slices a vector" $ do
-      previousNs 5 5 firstExample `shouldBe` V.fromList [35, 20, 15, 25, 47]
-      previousNs 5 14 firstExample `shouldBe` V.fromList [95, 102, 117, 150, 182]
+      previousNs 5 5 anExample `shouldBe` V.fromList [35, 20, 15, 25, 47]
+      previousNs 5 14 anExample `shouldBe` V.fromList [95, 102, 117, 150, 182]
 
     it "finds a matching sum" $ do
-      hasMatchingSum 5 firstExample 5 `shouldBe` True
-      hasMatchingSum 5 firstExample 6 `shouldBe` True
-      hasMatchingSum 5 firstExample 14 `shouldBe` False
+      hasMatchingSum 5 anExample 5 `shouldBe` True
+      hasMatchingSum 5 anExample 6 `shouldBe` True
+      hasMatchingSum 5 anExample 14 `shouldBe` False
+
+    it "finds possible slices" $ do
+      possibleSlices anExample 0 `shouldBe` [V.fromList [35]]
+      possibleSlices anExample 1 `shouldBe` map V.fromList [[20], [35, 20]]
+      possibleSlices anExample 2 `shouldBe` map V.fromList [[15], [20, 15], [35, 20, 15]]
+
+    it "knows if a continuous sum exists" $ do
+      findContiguousSum anExample 127 0 `shouldBe` []
+      findContiguousSum anExample 127 1 `shouldBe` []
+      findContiguousSum anExample 127 5 `shouldBe` [V.fromList [15, 25, 47, 40]]
 
