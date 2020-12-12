8982141bf4bf3e9a45b5155f70204de0795897fd
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-12 17:47:57 +0100

    Simplify template

--- src/DayXX.hs
+++ src/DayXX.hs
@@ -1,12 +1,7 @@
 module DayXX where
 
-import Data.Char
-
 main = interact $ unlines . (map show) . partOne . (map parseLine) . lines
 
-parseLine = map (read::String->Int) . words . map clean
-  where clean c
-          | isDigit c || isAlpha c = c
-          | otherwise = ' '
+parseLine = words
 
 partOne = id
--- test/DayXXSpec.hs
+++ test/DayXXSpec.hs
@@ -9,5 +9,5 @@ main = hspec spec
 spec =
   describe "Parsing a line" $ do
     it "reads a list of numbers" $ do
-      let result = parseLine "1: 2 3"
-      result `shouldBe` [1, 2, 3]
+      let result = parseLine "hello world"
+      result `shouldBe` ["hello", "world"]
