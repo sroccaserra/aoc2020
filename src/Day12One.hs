2f480b1a4f6ef731ca4473a764805e4a24be09f1
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-12 12:17:58 +0100

    r Use complex numbers for part two

--- src/Day12One.hs
+++ src/Day12One.hs
@@ -10,13 +10,13 @@ parseLine s = error $ "wrong input: " ++ s
 partOne = manhattanDistance . foldl moveShip (1:+0, 0:+0)
   where manhattanDistance (_, x:+y) = abs x + abs y
 
-j = 0 :+ 1
+j = 0:+1
 
-moveShip (d,p) ('N',x) = (d, (0 :+ x) + p)
-moveShip (d,p) ('S',x) = (d, (0 :+ (-x)) + p)
-moveShip (d,p) ('E',x) = (d, (x :+ 0) + p)
-moveShip (d,p) ('W',x) = (d, ((-x) :+ 0) + p)
-moveShip (d,p) ('F',x) = (d, (x :+ 0)*d + p)
-moveShip (d,p) ('R',x) = ((-j)^(div (round x) 90)*d, p)
-moveShip (d,p) ('L',x) = (j^(div (round x) 90)*d, p)
+moveShip (d,p) ('N',x) = (d, (0:+x) + p)
+moveShip (d,p) ('S',x) = (d, -(0:+x) + p)
+moveShip (d,p) ('E',x) = (d, (x:+0) + p)
+moveShip (d,p) ('W',x) = (d, -(x:+0) + p)
+moveShip (d,p) ('F',x) = (d, (x:+0) * d + p)
+moveShip (d,p) ('R',x) = ((-j)^(round x `div` 90)*d, p)
+moveShip (d,p) ('L',x) = (j^(round x `div` 90)*d, p)
 moveShip _ _ = error "wrong instruction"
--- src/Day12Two.hs
+++ src/Day12Two.hs
@@ -1,26 +1,22 @@
 module Day12Two where
 
+import Data.Complex
+
 main = interact $ show . partTwo . (map parseLine) . lines
 
-parseLine (d:x) = (d,read x ::Int)
+parseLine (d:x) = (d,read x ::Float)
 parseLine s = error $ "wrong input: " ++ s
 
-partTwo = manhattanDistance . foldl moveShip (10,1,0,0)
-  where manhattanDistance (_,_, x, y) = abs x + abs y
+partTwo = manhattanDistance . foldl moveShip (10:+1, 0:+0)
+  where manhattanDistance (_, x:+y) = abs x + abs y
 
-moveShip (u,v,x,y) ('N',n) = (u,v+n,x,y)
-moveShip (u,v,x,y) ('S',n) = (u,v-n,x,y)
-moveShip (u,v,x,y) ('E',n) = (u+n,v,x,y)
-moveShip (u,v,x,y) ('W',n) = (u-n,v,x,y)
-moveShip s ('F',n) = advanceShip n s
-moveShip s ('R',a) = turnShipRight a s
-moveShip s ('L',a) = turnShipLeft a s
+j = 0:+1
+
+moveShip (w,p) ('N',x) = (w + (0:+x), p)
+moveShip (w,p) ('S',x) = (w - (0:+x), p)
+moveShip (w,p) ('E',x) = (w + (x:+0), p)
+moveShip (w,p) ('W',x) = (w - (x:+0), p)
+moveShip (w,p) ('F',x) = (w, (x:+0)*w+p)
+moveShip (w,p) ('R',x) = ((-j)^(round x `div` 90)*w, p)
+moveShip (w,p) ('L',x) = (j^(round x `div` 90)*w, p)
 moveShip _ _ = error "wrong instruction"
-
-advanceShip n (u,v,x,y) = (u,v,x+n*u, y+n*v)
-
-turnShipRight a (u,v,x,y) | a > 90 = turnShipRight (a-90) (v,-u,x,y)
-turnShipRight _ (u,v,x,y) = (v,-u,x,y)
-
-turnShipLeft a (u,v,x,y) | a > 90 = turnShipLeft (a-90) (-v,u,x,y)
-turnShipLeft _ (u,v,x,y) = (-v,u,x,y)
