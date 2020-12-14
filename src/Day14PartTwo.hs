d1cd86c8592d43a2b59ae6ad85598fbc418ef78f
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-14 20:09:48 +0100

    r Reorganise code

--- src/Day14PartTwo.hs
+++ src/Day14PartTwo.hs
@@ -31,34 +31,36 @@ parseInstructions = map (toInstruction . words . map clean)
         toInstruction (x:y:_) = (read x, read y)
         toInstruction _ = error "wrong instruction"
 
-partTwo = sum . M.elems . foldl applyMaskWithInstruction initMemory
+partTwo = sum . M.elems . foldl applyMaskWithInstruction M.empty
+
+type Memory = M.Map Int64 Int64
+
+type Patch = [BitPatch]
+type BitPatch = (Int,Int64)
+
+type BitList = [Int64]
+
+type MaskWithInstructions = (MaskPair, [MemInstruction])
+type MaskPair = (Int64, String)
+type MemInstruction = (Int64, Int64)
 
 applyMaskWithInstruction :: Memory ->  MaskWithInstructions -> Memory
 applyMaskWithInstruction m (maskPair, xs) = updateMemValues m (maskInstructions maskPair xs)
 
 maskInstructions :: MaskPair -> [MemInstruction] -> [MemInstruction]
-maskInstructions (om, xm) xs = concatMap (applyXMask xm) $ map (applyOMask om) xs
+maskInstructions (om, xm) = concatMap (applyXMask xm) . map (applyOrMask om)
 
-applyOMask mask (a, v) = (mask .|. a, v)
+applyOrMask mask (a, v) = (mask .|. a, v)
 
 applyXMask :: String -> MemInstruction -> [MemInstruction]
 applyXMask mask (a, v) = map (\x -> (x,v)) $ floatedAddresses addressBits patches
-  where xIndices = map (+ (64-36)) $ findIndices (== 'X') mask
+  where xIndices = findIndices (== 'X') mask
         patches :: [Patch]
         patches = map (zip xIndices) $ comb $ length xIndices
         addressBits :: BitList
         addressBits = toBin a
         floatedAddresses :: BitList -> [Patch] -> [Int64]
         floatedAddresses bs ps = map toDec $ map (applyPatch bs) ps
-
-comb 0 = [[]]
-comb n = map (0:) rest ++ map (1:) rest
-  where rest = comb (n-1)
-
-type Patch = [BitPatch]
-type BitPatch = (Int,Int64)
-
-type BitList = [Int64]
 
 applyPatch :: BitList -> Patch -> BitList
 applyPatch bs p = foldl replaceBits bs p
@@ -69,26 +71,21 @@ replaceBits bs (n,b) = h ++ b:t
 
 updateMemValues :: Memory -> [MemInstruction] -> Memory
 updateMemValues m xs = foldl updateMemValue m xs
-  where updateMemValue :: M.Map Int64 Int64 -> MemInstruction -> M.Map Int64 Int64
+  where updateMemValue ::Memory -> MemInstruction -> Memory
         updateMemValue m (a, v) = M.insert a v m
 
-initMemory :: Memory
-initMemory = M.empty
+comb 0 = [[]]
+comb n = map (0:) rest ++ map (1:) rest
+  where rest = comb (n-1)
 
-type MaskWithInstructions = (MaskPair, [MemInstruction])
-type MaskPair = (Int64, String)
-type MemInstruction = (Int64, Int64)
-
-type Memory = M.Map Int64 Int64
-
-toDec :: [Int64] -> Int64
+toDec :: BitList -> Int64
 toDec = foldl1 $ (+) . (*2)
 
 toBin :: Int64 -> BitList
 toBin 0 = [0]
 toBin n = padding ++ bits
   where bits = reverse (toBin' n)
-        padding = replicate (64 - length bits) 0
+        padding = replicate (36 - length bits) 0
 
 toBin' 0 = []
 toBin' n = let (q,r) = n `divMod` 2 in r : toBin' q
