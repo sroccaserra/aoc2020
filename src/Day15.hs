36a7a78795b6dc6fa5372294028d4defa5a5aa12
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-15 11:29:38 +0100

    r Remove use of vector for part one

--- src/Day15.hs
+++ src/Day15.hs
@@ -10,22 +10,27 @@ main = interact $ show . partOne . parseLine
 parseLine :: String -> [Int]
 parseLine = map read . splitOn ","
 
-partOne ns = v V.! (pred $ V.length v)
-  where (v,_,_) = step $ initMap ns 2020
+partOne ns = (p, i, k)
+  where (v,m,k) = initMap ns 2020
+        (_,p,i,_) = step (m, v V.! (pred $ V.length v), V.length v, k)
 
-type State = (V.Vector Int, M.Map Int Int, Int)
+type InitState = (V.Vector Int, M.Map Int Int, Int)
 
-initMap :: [Int] -> Int -> State
+-- m p i k
+type State = (M.Map Int Int, Int, Int, Int)
+
+initMap :: [Int] -> Int -> InitState
 initMap ns@(h:_) k = foldl acc (V.fromList [h], M.empty, k) [2..length ns]
-  where acc :: State -> Int -> State
+  where acc :: InitState -> Int -> InitState
         acc (v,m,k) i = let n = ns !! (i-1) ; p = ns !! (i-2) in (V.snoc v n, M.insert p (i-1) m, k)
 initMap _ _ = error "wrong input"
 
 step :: State -> State
-step st@(v,_,k) | V.length v == k = st
-step (v,m,k) = step (V.snoc v n, M.insert p i m, k)
-  where i = V.length v
-        p = v V.! (i-1)
-        n = case M.lookup p m of
+step st@(_,_,i,k) | i == k = st
+step (m,p,i,k) = step (M.insert p i m, n, (succ i), k)
+  where n = case M.lookup p m of
                  Nothing -> 0
-                 Just j -> i-j -- trace (show (m, i, j, p)) i-j
+                 Just j -> i-j
+-- V.length v -> i
+-- v V.! (i-1) -> p
+--
