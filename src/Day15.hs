8193746cf0d7f12f6f6952ed51645004c713d040
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-15 14:01:49 +0100

    r Remove use of vector

--- src/Day15.hs
+++ src/Day15.hs
@@ -3,32 +3,28 @@ module Day15 where
 import Data.List
 import Data.List.Split
 import qualified Data.Map as M
-import qualified Data.Vector as V
 
-main = interact $ show . partOne . parseLine
+main = interact $ show . partTwo . parseLine
 
 parseLine :: String -> [Int]
 parseLine = map read . splitOn ","
 
 partOne ns = p
   where n = 2020
-        (v,m) = initMap ns
-        (_,p,_) = foldl' step (m, v V.! (pred $ V.length v), V.length v) [1..n- (V.length v)]
+        m = initMap ns
+        (_,p,_) = foldl' step (m, ns !! (pred $ length ns), length ns) [1..n- (length ns)]
 
 partTwo ns = p
   where n = 30000000
-        (v,m) = initMap ns
-        (_,p,_) = foldl' step (m, v V.! (pred $ V.length v), V.length v) [1..n- (V.length v)]
+        m = initMap ns
+        (_,p,_) = foldl' step (m, ns !! (pred $ length ns), length ns) [1..n-(length ns)]
 
-type InitState = (V.Vector Int, M.Map Int Int)
 -- m p i
 type State = (M.Map Int Int, Int, Int)
 
-initMap :: [Int] -> InitState
-initMap ns@(h:_) = foldl acc (V.fromList [h], M.empty) [2..length ns]
-  where acc :: InitState -> Int -> InitState
-        acc (v,m) i = let n = ns !! (i-1) ; p = ns !! (i-2) in (V.snoc v n, M.insert p (i-1) m)
-initMap _ = error "wrong input"
+initMap :: [Int] -> M.Map Int Int
+initMap ns = foldl' acc M.empty [2..length ns]
+  where acc m i = let p = ns !! (i-2) in M.insert p (i-1) m
 
 step :: State -> Int -> State
 step (m,p,i) _ = (M.insert p i m, n, (succ i))
