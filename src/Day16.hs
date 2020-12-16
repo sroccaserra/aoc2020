60aa85b4738d385b72e3c6d40699746e9187d285
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-16 09:52:36 +0100

    [day 16] Solve part two (sort of)

--- src/Day16.hs
+++ src/Day16.hs
@@ -1,9 +1,10 @@
 module Day16 where
 
+import Control.Applicative
 import Data.Char
 import Data.List.Split
 
-main = interact $ show . partOne . parse
+main = interact $ unlines . (map show) . partTwo . parse
 
 parse xs = (map parseRule a, map parseTicket $ tail $ init c)
   where (a:_:c:_) = map (splitOn "\n") $ splitOn "\n\n" xs
@@ -19,9 +20,19 @@ parseTicket = map read . splitOn ","
 
 partOne (rules,tickets) = sum $ concat $ map (checkTicket rules) tickets
 
+partTwo (rules,tickets) = map (map fst) $ map (matchingRules rules) columns
+  where columns = transpose $ validTickets rules tickets
+
 checkTicket rs xs = filter (not . matchesAnyRule rs) xs
 
-matchesAnyRule rs x = any (matchesRule x) rs
+matchesAnyRule rs x = any (flip matchesRule x) rs
 
-matchesRule x (_,[a1,b1,a2,b2]) = (a1 <= x && x <= b1) || (a2 <= x && x <= b2)
+matchesRule (_,[a1,b1,a2,b2]) x = (a1 <= x && x <= b1) || (a2 <= x && x <= b2)
 matchesRule _ _ = error "wrong input"
+
+validTickets rs xs = filter (all $ matchesAnyRule rs) xs
+
+matchingRules rs xs = filter (\r -> all (matchesRule r) xs) rs
+
+transpose :: [[a]] -> [[a]]
+transpose = getZipList . traverse ZipList
