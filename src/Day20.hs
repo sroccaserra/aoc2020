ca63728e1cfb38342061427d72c61430e131dd13
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-20 08:14:25 +0100

    [day 20] Finds LTR and TTB matches

--- src/Day20.hs
+++ src/Day20.hs
@@ -12,7 +12,16 @@ labelId =
 line = munch1 (`elem` ".#")
 tiles = many1 (Tile <$> labelId <*> sepBy1 line (char '\n'))
 
-partOne = id
+partOne (a:b:_) = fitsLTR a b
 
 data Tile = Tile Int [String]
           deriving (Show)
+
+top (Tile _ (s:_)) = s
+bottom (Tile _ (xs)) = last xs
+left (Tile _ (xs)) = map head xs
+right (Tile _ (xs)) = map last xs
+
+fitsLTR a b = right a == left b
+
+fitsTTB a b = bottom a == top b
