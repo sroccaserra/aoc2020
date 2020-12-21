77bd8c9b94ebcb8a169b0a0d2f067249ccb9af1f
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-21 22:07:46 +0100

    [day 21] Solve alergens

--- src/Day21.hs
+++ src/Day21.hs
@@ -1,7 +1,10 @@
 module Day21 where
 
 import Data.Char
+import Data.List
+import Data.Maybe
 import Text.ParserCombinators.ReadP
+import qualified Data.Map as M
 
 main = interact $ unlines . (map show) . partOne . (map parseLine) . lines
 
@@ -16,4 +19,18 @@ parser = do
 word = munch1 isAlpha
 spaces = munch1 isSpace
 
-partOne = id
+partOne xs = map (\(a, [i]) -> (a,i)) $ solve [] hints
+  where alergens = nub $ concat $ map snd xs
+        m = foldl step M.empty xs
+        hints = map (\x -> (x,foldl1 intersect $ m M.! x)) alergens
+
+step m (is, as) = foldl (\acc a -> M.insertWith (++) a [is] acc) m as
+
+solve :: [(String, [String])] -> [(String, [String])] -> [(String, [String])]
+solve solved [] = solved
+solve solved toSolve = solve (next:solved) (map (removeIngredient solvedI) nextToSolve)
+  where next@(_, [solvedI]) = fromJust $ find (\(_,xs) -> (length xs) == 1) toSolve
+        nextToSolve = delete next toSolve
+
+removeIngredient :: String -> (String, [String]) -> (String, [String])
+removeIngredient i (a, is) = (a, filter (/= i) is)
