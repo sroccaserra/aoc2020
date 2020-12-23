de5dad35fb3abee61f48e33ba9fb6fe75125f01f
Sébastien Roccaserra <sroccaserra@yahoo.com>
2020-12-23 21:32:37 +0100

    [day 23] Convert to Sequence data structure

--- src/Day23.hs
+++ src/Day23.hs
@@ -1,31 +1,44 @@
 module Day23 where
 
 import Data.Char
-import Data.List
 import Data.Maybe
 import Text.ParserCombinators.ReadP
+import Data.Sequence (Seq(..),(<|),(|>),(><))
+import qualified Data.Sequence as Seq
+import Data.Foldable (toList)
 
-main = interact $ show . partOne . parse
+main = interact $ unlines . (map show) . partOne . parse
 
 parse s = fst $ last $ readP_to_S parser s
 
 parser :: ReadP [Int]
 parser = many1 $ read . (:[]) <$> (satisfy isDigit)
 
-partOne = concatMap show . tail . alignToOne . last . take 101 . iterate step
+partOne = concatMap show . Seq.drop 1 . alignToOne . last . take 101 . iterate step . Seq.fromList
 
-step (current:rest) = tail (concat [a, (take 3 rest), c]) ++ [current]
-  where subList = current : (drop 3 rest)
+partTwo xs = toList $ Seq.take 3 $ alignToOne $ last $ take 10000001 $ iterate step extended
+
+  where extended = Seq.fromList $ xs ++ [succ $ length xs..1000000]
+-- partTwo xs = map (Seq.take 20) $ take 10 $ iterate step extended
+--   where extended = Seq.fromList $ xs ++ [succ $ length xs..1000000]
+
+-- searchCycles i s xs | S.member xs s = (i,xs)
+-- searchCycles i s xs | 0 == mod i 1000 = trace (show i) $ searchCycles (succ i) (S.insert xs s) (step xs)
+-- searchCycles i s xs = trace (show i) $ searchCycles (succ i) (S.insert xs s) (step xs)
+
+step :: Seq Int -> Seq Int
+step (current :<| rest) = Seq.drop 1 (a >< (Seq.take 3 rest) >< c) |> current
+  where subList = current <| (Seq.drop 3 rest)
         destIndex = getDestIndex current subList
-        (a, c) = splitAt (succ destIndex) subList
+        (a, c) = Seq.splitAt (succ destIndex) subList
 step _ = error "list is too small?"
 
-getDestIndex :: Int -> [Int] -> Int
+getDestIndex :: Int -> Seq Int -> Int
 getDestIndex n subList =
-  if elem i' subList then fromJust $ elemIndex i' subList else getDestIndex i' subList
+  if elem i' subList then fromJust $ Seq.elemIndexL i' subList else getDestIndex i' subList
   where i = n - 1
         i' = if i == 0 then 9 else i
 
-alignToOne xs = end ++ begin
-  where i = fromJust $ elemIndex 1 xs
-        (begin, end) = splitAt i xs
+alignToOne xs = end >< begin
+  where i = fromJust $ Seq.elemIndexL 1 xs
+        (begin, end) = Seq.splitAt i xs
--- test/Day23Spec.hs
+++ test/Day23Spec.hs
@@ -1,6 +1,7 @@
 module Day23Spec where
 
 import Day23 hiding (main)
+import qualified Data.Sequence as Seq
 
 import Test.Hspec
 
@@ -8,28 +9,33 @@ main = hspec spec
 
 spec =
   describe "Crab game" $ do
-    it "matches the example" $ do
-      let result = take 10 $ iterate step [3,8,9,1,2,5,4,6,7]
-      result `shouldBe` [[3,8,9,1,2,5,4,6,7]
-                        ,[2,8,9,1,5,4,6,7,3]
-                        ,[5,4,6,7,8,9,1,3,2]
-                        ,[8,9,1,3,4,6,7,2,5]
-                        ,[4,6,7,9,1,3,2,5,8]
-                        ,[1,3,6,7,9,2,5,8,4]
-                        ,[9,3,6,7,2,5,8,4,1]
-                        ,[2,5,8,3,6,7,4,1,9]
-                        ,[6,7,4,1,5,8,3,9,2]
-                        ,[5,7,4,1,8,3,9,2,6]]
+    describe "Part one" $ do
+      it "matches the example" $ do
+        let result = take 10 $ iterate step $ Seq.fromList [3,8,9,1,2,5,4,6,7]
+        result `shouldBe` map Seq.fromList [[3,8,9,1,2,5,4,6,7]
+                                           ,[2,8,9,1,5,4,6,7,3]
+                                           ,[5,4,6,7,8,9,1,3,2]
+                                           ,[8,9,1,3,4,6,7,2,5]
+                                           ,[4,6,7,9,1,3,2,5,8]
+                                           ,[1,3,6,7,9,2,5,8,4]
+                                           ,[9,3,6,7,2,5,8,4,1]
+                                           ,[2,5,8,3,6,7,4,1,9]
+                                           ,[6,7,4,1,5,8,3,9,2]
+                                           ,[5,7,4,1,8,3,9,2,6]]
 
-    it "can place removed cups last" $ do
-      step [3,8,9,1,7,5,4,6,2] `shouldBe` [7,5,4,6,2,8,9,1,3]
+      it "can place removed cups last" $ do
+        (step $ Seq.fromList [3,8,9,1,7,5,4,6,2]) `shouldBe` Seq.fromList [7,5,4,6,2,8,9,1,3]
 
-    it "skips removed cups" $ do
-      step [9,8,7,6,1,5,4,3,2] `shouldBe` [1,5,8,7,6,4,3,2,9]
+      it "skips removed cups" $ do
+        (step $ Seq.fromList [9,8,7,6,1,5,4,3,2]) `shouldBe` Seq.fromList [1,5,8,7,6,4,3,2,9]
 
-    it "wraps around to 9" $ do
-      step [1,9,8,7,6,5,4,3,2] `shouldBe` [6,9,8,7,5,4,3,2,1]
+      it "wraps around to 9" $ do
+        (step $ Seq.fromList [1,9,8,7,6,5,4,3,2]) `shouldBe` Seq.fromList [6,9,8,7,5,4,3,2,1]
 
-    it "has the right 10th step" $ do
-      let result = last $ take 11 $ iterate step [3,8,9,1,2,5,4,6,7]
-      alignToOne result `shouldBe` [1,9,2,6,5,8,3,7,4]
+      it "has the right 10th step" $ do
+        let result = last $ take 11 $ iterate step $ Seq.fromList [3,8,9,1,2,5,4,6,7]
+        alignToOne result `shouldBe` Seq.fromList [1,9,2,6,5,8,3,7,4]
+
+    describe "Part two" $ do
+      it "can multiply big numbers" $ do
+        934001*159792 `shouldBe` 149245887792
